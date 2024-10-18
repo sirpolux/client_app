40cc64ce2059c9f8565d400c6cc395f9ac64b9e5
sirpolux <poluxreloaded@gmail.com>
2024-10-18 09:59:50 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ClientComponent } from './client/client.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'client',
+        component:ClientComponent
+    },
+    {
+        path:'sign-up',
+        component:SignUpComponent
+    }
+];
---
+++ src/app/new-client/new-client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-client',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-client.component.html',
+  styleUrl: './new-client.component.scss'
+})
+export class NewClientComponent {
+
+}
