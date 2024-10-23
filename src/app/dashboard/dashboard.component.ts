d7fd4b9920ad0b7161fcccb89d676ab6c70896dc
sirpolux <poluxreloaded@gmail.com>
2024-10-23 07:54:07 +0100

    update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -28,16 +28,17 @@ export class DashboardComponent {
 
   
     clientData:ClientRepresentation[]=[
-        {name: "Peter Mike", accountNumber: "0044347789", description: "Some of our clients are here"},
-        {name: "Peter Mike", accountNumber: "0044347789", description: "Some of our clients are here"},
-        {name: "Peter Mike", accountNumber: "0044347789", description: "Some of our clients are here"},
-        {name: "Peter Mike", accountNumber: "0044347789", description: "Some of our clients are here"},
-        {name: "Peter Mike", accountNumber: "0044347789", description: "Some of our clients are here"},
-        {name: "Peter Mike", accountNumber: "0044347789", description: "Some of our clients are here"},
-        {name: "Peter Mike", accountNumber: "0044347789", description: "Some of our clients are here"},
-        {name: "Peter Mike", accountNumber: "0044347789", description: "Some of our clients are here"},
-        {name: "Peter Mike", accountNumber: "0044347789", description: "Some of our clients are here"},
-        {name: "Peter Mike", accountNumber: "0044347789", description: "Some of our clients are here"}
+      { name: "Peter Mike", accountNumber: "0044347789", description: "Client specializing in retail." },
+      { name: "Alice Johnson", accountNumber: "0044347790", description: "Client focused on e-commerce." },
+      { name: "John Doe", accountNumber: "0044347791", description: "Client in the tech industry." },
+      { name: "Mary Smith", accountNumber: "0044347792", description: "Client managing a health service." },
+      { name: "Tom Brown", accountNumber: "0044347793", description: "Client running a local bakery." },
+      { name: "Linda Green", accountNumber: "0044347794", description: "Client offering online courses." },
+      { name: "Chris White", accountNumber: "0044347795", description: "Client with a consulting firm." },
+      { name: "Sarah Black", accountNumber: "0044347796", description: "Client in the automotive sector." },
+      { name: "James Grey", accountNumber: "0044347797", description: "Client specializing in finance." },
+      { name: "Laura Blue", accountNumber: "0044347798", description: "Client operating a travel agency." }
+  
     ]
 
     
